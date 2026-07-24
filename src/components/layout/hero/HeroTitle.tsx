a23e16acbf11bafc9a5aa154efddb5f90be33c56
BuiAn <sekiro961@gmail.com>
2026-07-24 22:17:48 +0700

    heri title

--- src/components/layout/hero/HeroTitle.tsx
+++ src/components/layout/hero/HeroTitle.tsx
@@ -1,3 +1,13 @@
 export default function HeroTitle() {
-    return <div>Transform your long links into powerful URLs</div>
+    return( 
+    <div>
+        <div className="max-w-5xl text-6xl font-extrabold leading-tight text-white md:text-7xl">
+        Transform your {" "}
+        <span className="text-sky-400">
+        long links
+        </span>{" "}
+        into powerful URLs
+        </div>
+    </div>
+);
 }
