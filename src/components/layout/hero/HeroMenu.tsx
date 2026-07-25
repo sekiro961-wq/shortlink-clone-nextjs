afcff44fdb8edc49a1e6b0464cef8a4424f4796d
BuiAn <sekiro961@gmail.com>
2026-07-25 16:59:33 +0700

    paste URL bar

--- src/components/layout/hero/HeroMenu.tsx
+++ src/components/layout/hero/HeroMenu.tsx
@@ -1,3 +1,29 @@
 export default function HeroMenu() {
-    return <div>Menu</div>
+    return (
+        <div className="mt-14 w-full max-w-5xl rounded-3xl border border-slate-800 bg-slate-900/70 p-6 backdrop-blur">
+            <div className="flex flex-col gap-4 lg:flex-row">
+                <div className="flex flex-col gap-4 lg:flex-row">
+                    <input
+                        type="text"
+                        placeholder="Paste your URL here..."
+                        className="
+                        h-16
+                        w-full
+                        rounded-2xl
+                        border
+                        border-slate-700
+                        bg-slate-950
+                        px-5
+                        pr-24
+                        text-white
+                        outline-none
+                        placeholder:text-slate-500
+                        focus:border-sky-500
+                    "
+                    >
+                    </input>
+                </div>
+            </div>
+        </div>
+    );
 }
--- src/components/layout/hero/HeroSubtitle.tsx
+++ src/components/layout/hero/HeroSubtitle.tsx
@@ -1,6 +1,6 @@
 export default function HeroSubtitle() {
     return (
-        <div className="text-xl text-white mb-8 max-w-2xl mx-auto opacity-80">
+        <div className="text-xl text-white mb-8 max-w-2xl mx-auto opacity-80 text-center">
             Shorten, customize and track your links with advanced analytics. The complete solution to manage your digital campaigns.
         </div>
     );
