8985f267a868e254fca95f67e26b52bad987830a
BuiAn <sekiro961@gmail.com>
2026-07-25 21:39:14 +0700

    logo link

--- src/components/layout/navbar/NavbarLogo.tsx
+++ src/components/layout/navbar/NavbarLogo.tsx
@@ -1,19 +1,21 @@
+import { Link } from 'lucide-react';
 export default function NavbarLogo() {
   return (
-    <a  href="#" className="flex items-center gap-3 hover: opacity-80">
-      <div className="flex h-10 w-10 items-center justify-center rounded-lg bg-gradient-to-r from-violet-600 to-blue-500">
+    <a href="#" className="hover: flex items-center gap-3 opacity-80">
+      <div className="relative h-10 w-10">
+        <Link
+          size={24}
+          strokeWidth={3}
+          className="absolute top-2 left-0 rotate-5 text-violet-500"
+        />
       </div>
       <div>
         <h1 className="text-xl font-bold text-white">
           <span className="text-violet-500">Link</span>
           <span className="text-blue-400">Short</span>
         </h1>
-          <p className="-mt-1 text-[10px] tracking-[2px] text-slate-400">
-            SMART LINKS
-          </p>
+        <p className="-mt-1 text-[10px] tracking-[2px] text-slate-400">SMART LINKS</p>
       </div>
-        
-
-    </a> 
+    </a>
   );
 }
